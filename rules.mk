eee7f747e43cbae207832d5b9d95004cc20b68c0
Adam H. Sparks <adamhsparks@icloud.com>
2023-03-11 17:40:20 +0800

    Try to see if this will compile

--- rules.mk
+++ rules.mk
@@ -1,1 +1,14 @@
-SRC += source.c
+
+VIA_ENABLE = yes 
+ENCODER_MAP_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+
+SRC += keymap_user_config.c
+
+ifeq ($(strip $(RGB_MATRIX_ENABLE)), yes)
+    SRC += rgb_matrix_user.c
+endif
+
+VPATH += keyboards/keychron/common
+SRC += keychron_common.c
+SRC += keychron_factory_test_common.c
